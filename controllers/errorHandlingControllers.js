9ddc70d7c0f1ceb03c5ad2f7db9ffd9b30f83c09
Melissa Nzekele Courtney <melissanzekelecourtney@gmail.com>
2023-02-23 09:34:28 +0000

    errors handled for ticket 07

--- controllers/errorHandlingControllers.js
+++ controllers/errorHandlingControllers.js
@@ -5,6 +5,10 @@ exports.handle404Status = (request, response) => {
 exports.handlePSQL400Status = (error, request, response, next) => {
   if (error.code === "22P02") {
     response.status(400).send({ msg: "Bad Request" });
+  } else if (error.code === "23503") {
+    response.status(400).send({ msg: "Article Does Not Exist" });
+  } else if (error === "Invalid Comment Format") {
+    response.status(400).send({ msg: "Invalid Comment Format" });
   } else {
     next(error);
   }
@@ -15,7 +19,10 @@ exports.handleCustomErrors = (error, request, response, next) => {
     response.status(404).send({ msg: "Article Not Found" });
   } else if (error === "Comment Not Found") {
     response.status(404).send({ msg: "Comment Not Found" });
+  } else if (error === "Author Not Found") {
+    response.status(404).send({ msg: "Author Not Found" });
   } else {
-    next(error);
+    console.log(error);
+    response.status(500).send({ msg: "Internal Server Error" });
   }
 };
